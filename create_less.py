ca189a7cd94593de4e7fd530ae919733f82313b3
Ryan Foster <ryan@rynamic.com>
2011-12-21 15:14:39 -0800

    added python to generate .less file to apply namespace to all bootstrap styles

---
+++ create_less.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+def process_file():
+    
+    f = open('bootstrap/bootstrap.css', 'r')
+    output = open('gwui.less', 'w')
+    output.write('.gwui {\n')
+    for line in f:
+    	output.write(line)
+    output.write('\n}')
+    f.close()
+    output.close()
+
+def main():
+	process_file()
+
+if __name__ == '__main__':
+    sys.exit(main())
